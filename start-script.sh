7eb80112b42943ed10b342a72bd375cdca435529
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2021-07-23 16:58:57 -0300

    Add files via upload

--- start-script.sh
+++ start-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Bqj4Hgukj3MSzMJOXMlh78KUxhBViPXP' -O debian-post-install.tar
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Bqj4Hgukj3MSzMJOXMlh78KUxhBViPXP' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Bqj4Hgukj3MSzMJOXMlh78KUxhBViPXP" -O debian-post-install.tar && rm -rf /tmp/cookies.txt
 
 tar -xvf debian-post-install.tar
 
