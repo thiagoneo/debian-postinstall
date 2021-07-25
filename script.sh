0d124d7573f36b73fc50420dc41c0e4a3332b7fd
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2021-07-25 16:10:49 -0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+
+# ------------------------------- VARIÁVEIS -----------------------------------#
 SCR_DIRECTORY=`pwd`
 
 PACOTES="slick-greeter lightdm lightdm-settings xserver-xorg-core xserver-xorg-video-intel \
@@ -18,12 +20,24 @@ hunspell-pt-br hyphen-pt-br myspell-pt-br gstreamer1.0-plugins-good \
 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav \
 gparted file-roller package-update-indicator gnome-packagekit squashfs-tools \
 gstreamer1.0-vaapi libavcodec-extra zstd zip unzip rar unrar unar brave-browser \
-qt5ct qt5-style-plugins flatpak"
+qt5ct qt5-style-plugins flatpak ntp libreoffice-core libreoffice-calc \
+libreoffice-impress libreoffice-writer libreoffice-l10n-pt-br \
+libreoffice-lightproof-pt-br libreoffice-help-pt-br libreoffice-gtk3 default-jre clang \
+build-essential ttf-mscorefonts-installer unalz arj lhasa lzip lzop ncompress \
+rzip unace gpart mtools xfsprogs fonts-3270 fonts-comfortaa fonts-ibm-plex \
+fonts-league-spartan fonts-lindenhill fonts-mplus fonts-paratype fonts-rufscript \
+fonts-prociono fonts-tomsontalks fonts-vollkorn fonts-sil-andika \
+fonts-sil-gentium-basic fonts-sil-gentiumplus fonts-sil-gentiumplus-compact \
+fonts-noto-core fonts-noto-cjk gdebi"
 
 PACOTES_SEM_RECOMMENDS="evince gnome-calendar gnome-calculator geary gnome-2048 \
-gnome-mines gnome-nibbles supertux kolourpaint baobab"
+gnome-mines gnome-nibbles supertux kolourpaint baobab thunar-font-manager"
 
-#----------------------CONFIGURAR REPOSITÓRIOS ------------------------#
+PACOTES_PARA_REMOVER="evolution-data-server firefox-esr at-spi2-core bolt fwupd \
+kwayland-data kwayland-integration qtwayland5 sane-utils tango-icon-theme telnet"
+
+
+#------------------------- CONFIGURAR REPOSITÓRIOS ----------------------------#
 sudo apt update
 while :; do sudo -v; sleep 59; done &
 infiloop=$!
@@ -39,7 +53,7 @@ echo 'deb-src http://deb.debian.org/debian bullseye-updates main contrib non-fre
 
 sudo apt update
 
-### Repositório do navegador Brave ###
+#### Repositório do navegador Brave ####
 cd $SCR_DIRECTORY/
 sudo apt install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -47,8 +61,7 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=
 sudo apt update
 
 
-#----------------------- INSTALAR PACOTES--------------------------------#
-
+#---------------------------- INSTALAR PACOTES --------------------------------#
 echo ""
 echo "INICIANDO ATUALIZAÇÃO COMPLETA DO SISTEMA..."
 echo ""
@@ -60,20 +73,23 @@ echo ""
 sudo apt install $PACOTES -y
 sudo apt install $PACOTES_SEM_RECOMMENDS -y
 
-### Instalação de pacotes locais ###
+##### Instalação de pacotes locais #####
 sudo dpkg -i $SCR_DIRECTORY/local-packages/*.deb
-sudo apt install -f
+sudo apt install -f -y
+
+#------------------- DESINSTALAR PACOTES DESNECESSÁRIOS -----------------------#
+sudo apt purge $PACOTES_PARA_REMOVER -y
+sudo apt autoremove --purge -y
 
 
-#------------------- CONFIGURAR ARQUIVOS DO SISTEMA -----------------------#
+#--------------------- CONFIGURAR ARQUIVOS DO SISTEMA -------------------------#
 cd $HOME
 sudo chown -R root:root $SCR_DIRECTORY/system-files/
 cd $SCR_DIRECTORY/
 sudo cp -rp $SCR_DIRECTORY/system-files/etc/skel/ /etc/
 sudo \cp -rf $SCR_DIRECTORY/system-files/etc/lightdm/ /etc/
-sudo \cp -r $SCR_DIRECTORY/system-files/etc/drirc /etc/
-sudo \cp -r $SCR_DIRECTORY/system-files/etc/default/grub /etc/default/grub
-sudo \cp -r $SCR_DIRECTORY/system-files/usr/share/applications/gdebi.desktop /usr/share/applications/
+sudo \cp $SCR_DIRECTORY/system-files/etc/drirc /etc/
+sudo \cp $SCR_DIRECTORY/system-files/etc/default/grub /etc/default/grub
 echo "vm.swappiness=25" | sudo tee -a /etc/sysctl.conf
 echo "vm.vfs_cache_pressure=50" | sudo tee -a /etc/sysctl.conf
 echo "vm.dirty_background_ratio=5" | sudo tee -a /etc/sysctl.conf
@@ -88,18 +104,23 @@ sudo systemctl set-default graphical.target
 sudo systemctl enable ufw
 sudo ufw enable
 sudo plymouth-set-default-theme -R homeworld
+###### Para garantir que a configuração do lightdm tenha efeito ######
+sudo dpkg-reconfigure lightdm lightdm-settings slick-greeter 
+sudo dpkg-reconfigure numlockx 
 
 
-############### Configuração do Flatpak #############
+#-------------------------- CONFIGURAÇÃO DO FLATPAK ---------------------------#
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-############### Configuração do usuário #############
+
+#-------------------------- CONFIGURAÇÃO DO USUÁRIO ---------------------------#
 sudo cp -rp /etc/skel/.config/ /etc/skel/.local/ $HOME/
 sudo chown -R $USER:$USER $HOME/.config $HOME/.local
 
 kill "$infiloop"
 
-#################### Reiniciar ######################
+
+#------------------------------------ FIM -------------------------------------#
 clear
-echo "Reiniciando..."
-sudo reboot
+echo "Chegamos ao fim."
+echo "Você pode reiniciar agora com o comando '/sbin/reboot'."
