2b2264073781bc08a5f96496bec41feaab232643
Thiago Sousa <thiagosousa77@outlook.com>
2021-12-26 12:01:31 -0300

    update

--- pos-reboot.sh
+++ pos-reboot.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 ########### Instalação de pacotes flatpak #############
-flatpak install flathub com.github.tchx84.Flatseal com.snes9x.Snes9x org.geogebra.GeoGebra org.stellarium.Stellarium de.haeckerfelix.Fragments
+sudo echo "Iniciando instalação de Flatpaks"
+while :; do sudo -v; sleep 59; done &
+infiloop=$!
+
+flatpak install flathub $(cat lista-flatpaks.txt)
 
 ########## Instalação de temas para apps flatpak ######
 sudo apt install ostree appstream-util -y
 cd $HOME
-git clone https://github.com/refi64/pakitheme.git
-cd pakitheme
-sudo bash pakitheme install-system Fluent
-sudo bash pakitheme install-system Fluent-light
-sudo bash pakitheme install-system Fluent-dark
+git clone https://github.com/refi64/stylepak.git
+cd stylepak
+bash stylepak install-system Fluent
+bash stylepak install-system Fluent-compact
+bash stylepak install-system Fluent-dark
+bash stylepak install-system Fluent-dark-compact
+bash stylepak install-system Fluent-light
+bash stylepak install-system Fluent-light-compact
+bash stylepak install-system Fluent-round
+bash stylepak install-system Fluent-round-compact
+bash stylepak install-system Fluent-round-dark
+bash stylepak install-system Fluent-round-dark-compact
+bash stylepak install-system Fluent-round-light
+bash stylepak install-system Fluent-round-light-compact
+
+kill "$infiloop"
 
 echo "Pronto."
--- start-script.sh
+++ start-script.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Bqj4Hgukj3MSzMJOXMlh78KUxhBViPXP' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Bqj4Hgukj3MSzMJOXMlh78KUxhBViPXP" -O debian-post-install.tar && rm -rf /tmp/cookies.txt
-
-tar -xvf debian-post-install.tar
-
-cd debian-post-install/
-
-wget -c https://raw.githubusercontent.com/thiagoneo/debian-postinstall/main/script.sh
-wget -c https://raw.githubusercontent.com/thiagoneo/debian-postinstall/main/pos-reboot.sh
-
-chmod +x script.sh
-chmod +x pos-reboot.sh
+wget https://github.com/thiagoneo/debian-postinstall/archive/refs/tags/2021-12-26.tar.gz
+tar -xzvf 2021-12-26.tar.gz
+cd debian-postinstall-2021-12-26/
+chmod +x *.sh
 
 bash script.sh
