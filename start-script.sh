e8af0714bf8e0d3dc87aa17f1feb83dc6dff0653
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2022-02-27 00:21:35 +0000

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://github.com/thiagoneo/debian-postinstall/archive/refs/tags/2022-02-19-2.tar.gz
-tar -xzvf 2022-02-19-2.tar.gz
-cd debian-postinstall-2022-02-19-2/
+wget https://github.com/thiagoneo/debian-postinstall/archive/refs/tags/2022-02-27.tar.gz
+tar -xzvf 2022-02-27.tar.gz
+cd debian-postinstall-2022-02-27/
 chmod +x *.sh
 
 bash script.sh
